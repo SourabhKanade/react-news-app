0ca37013b4f28b5b66a54adaa99c73b262a1dbd3
Sourabh <kanadesourabh420@gmail.com>
2021-09-04 00:25:01 +0530

    second update

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 export class NewsItem extends Component {
     render() {
-        let {title, description, imageURL, URL} = this.props; 
+        let {title, description, imageURL, newsUrl} = this.props; 
                 return (
          <div>
-            <div className="card" style={{width: "18rem"}}>
-                <img src={imageURL} className="card-img-top" alt="img"/>
+            <div className="card border-primary" style={{width: "18rem"}}>
+                <img src= {imageURL?imageURL:"https://aniportalimages.s3.amazonaws.com/media/details/__sized__/Concussionjune2820210903073948-thumbnail-154x87-70.jpg"} className="card-img-top" alt="img"/>
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href="/" className="btn btn-sm btn-primary">Read More</a>
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <a rel="noreferrer" href={newsUrl} target= "_blank" className="btn btn-sm css-button-arrow--blue">Read More</a>
                 </div>
             </div>
-        </div>        
+        </div>
         )
     }
 }
