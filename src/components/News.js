97387965ee0350eb118bd0f796ac5d0fef565d4d
Sourabh <kanadesourabh420@gmail.com>
2021-09-05 13:46:22 +0530

    prev-next buttons updated

--- src/components/News.js
+++ src/components/News.js
@@ -7,24 +7,23 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
-    }
+      page: 1,
+    };
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=1pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=1&pageSize=21`;
     let data = await fetch(url);
-    let parsedData = await data.json()
+    let parsedData = await data.json();
     // console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
-      totalArticles: parsedData.totalResults
-    })
+      totalResults: parsedData.totalResults,
+    });
   }
 
   handlePrevClick = async () => {
-    console.log("Prev");
+    console.log("Prev Page");
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=${
       this.state.page - 1
     }&pageSize=20`;
@@ -38,7 +37,7 @@ export class News extends Component {
   };
 
   handleNextClick = async () => {
-    console.log("Next");
+    console.log("Next Page");
     if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
     } else {
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=${
@@ -54,25 +53,30 @@ export class News extends Component {
     }
   };
 
-  render() {
+ render() {
+   
     return (
       <div className="container my-4 text-center">
         <h1>Ninja News - Top Headlines</h1>
         <div className="row my-4">
           {this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem
-                title={element.title ? element.title.slice(0, 45) : ""}
-                description={element.description ? element.description.slice(0, 100) : ""
-                }
-                imageURL={element.urlToImage}
-                newsUrl={element.url}
-              />
-            </div>
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 100) : ""
+                  }
+                  imageURL={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
+            disabled={this.state.page <= 1}
             type="button"
             onClick={this.handlePrevClick}
             className="btn css-button-sliding-to-left--sky"
@@ -93,6 +97,5 @@ export class News extends Component {
     );
   }
 }
-export default News;
 
-// disabled={this.state.page <= 1}
+export default News
