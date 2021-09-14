a4888d7e06ea360d3884c2e4895f185bea152ebe
Sourabh <kanadesourabh420@gmail.com>
2021-09-14 14:21:01 +0530

    func. modified

--- src/components/News.js
+++ src/components/News.js
@@ -6,9 +6,10 @@ import PropTypes from "prop-types";
 export class News extends Component {
   static defaultProps = {
     country: "in",
-    pageSize: 9,
-    // category: general
+    pageSize: 12,
+    category: 'general'
   };
+
   static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
@@ -19,67 +20,37 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: true,
-      page: 1,
+      loading: false,
+      page: 1
     };
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  async updateNews(){
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false,
+      loading: false
     });
   }
 
+    async componentDidMount() {
+      this.updateNews();
+   }
+
   handlePrevClick = async () => {
     console.log("Prev Page");
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
+    this.setState({page: this.state.page - 1});
+    this.updateNews();
   };
 
   handleNextClick = async () => {
     console.log("Next Page");
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.props.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=82323b612ace4e23ae7aa8f574d1d708&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      // console.log(parsedData);
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
-    }
+    this.setState({page: this.state.page + 1});
+    this.updateNews();
   };
 
   render() {
@@ -93,8 +64,18 @@ export class News extends Component {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
-                    title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 100): ""}
-                    urlToImage={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 100)
+                        : ""
+                    }
+                    urlToImage={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
                 </div>
               );
             })}
@@ -104,7 +85,8 @@ export class News extends Component {
             disabled={this.state.page <= 1}
             type="button"
             onClick={this.handlePrevClick}
-            className="btn css-button-sliding-to-left--sky">
+            className="btn css-button-sliding-to-left--sky"
+          >
             {" "}
             &larr; Previous{" "}
           </button>
